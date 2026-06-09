7dfa47d677474bc48d256849ef6a88276bf01bf6
868meshbot <cryptocracker99@gmail.com>
2026-06-09 09:24:13 +0100

    Fix duplicate case 32: renumber Extended Latin Font row to 33

--- src/ui/ScreenSettings.cpp
+++ src/ui/ScreenSettings.cpp
@@ -224,7 +224,7 @@ void ScreenSettings::_buildList(lv_obj_t* parent) {
     };
     int themeIdx = (cfg.theme >= 0 && cfg.theme < theme::THEME_COUNT) ? cfg.theme : 0;
     _addRow(_list, "Theme", kThemeNames[themeIdx], 31);
-    _addRow(_list, "Extended Latin Font", cfg.fontExtLatin  ? "On" : "Off", 32);
+    _addRow(_list, "Extended Latin Font", cfg.fontExtLatin  ? "On" : "Off", 33);
     _addRow(_list, "Bluetooth",       cfg.bluetoothEnabled ? "On" : "Off", 7);
     _addRow(_list, "Speaker",         cfg.speakerEnabled   ? "On" : "Off", 8);
     static const char* gpsModeNames[] = { "Off", "Intermittent", "On" };
@@ -3149,7 +3149,7 @@ void ScreenSettings::_onItemClick(lv_event_t* e) {
             _openThemeDialog();
             return;
 
-        case 32:  // Extended Latin Font
+        case 33:  // Extended Latin Font
             cfg.fontExtLatin = !cfg.fontExtLatin;
             break;
 
